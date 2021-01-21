2c55b2255fcd2fa37a84c9f4fcd2cd8c2fa57f72
kaslon <matferraro@gmail.com>
2021-01-21 14:48:42 -0500

    week2 end of class thursday

---
+++ Week2/models/Game.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var GameSchema = new Schema(
+    {
+        game:
+        {
+            type:String,
+            required:true
+        }
+    })
+    
+
+    
+    mongoose.model('game', GameSchema)
