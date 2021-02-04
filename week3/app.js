ee8867d52f7d744a84b900f67e9320f8374f4380
kaslon <matferraro@gmail.com>
2021-02-04 14:29:26 -0500

    end of week 4

--- week3/app.js
+++ week3/app.js
@@ -2,6 +2,7 @@ var express = require("express")
 var app = express()
 var serv = require("http").Server(app)
 var io = require("socket.io")(serv,{})
+var debug = true
 
 //file communication
 app.get('/', function(req, res)
@@ -17,45 +18,234 @@ serv.listen(3000, function()
 })
 
 var SocketList = {}
-var PlayerList = {}
+//var PlayerList = {}
 
-var Player = function(id)
+// class for a gameobject
+var GameObject = function()
 {
     var self = 
     {
         x:400,
         y:300,
-        id:id,
-        number: Math.floor(Math.random()* 10),
-        up: false,
-        down: false,
-        right: false,
-        left: false,
-        speed: 10,
+        spX:0,
+        spY:0,
+        id:"",
+       
     }
+    self.update = function()
+    {
+        self.updatePosition()
+    }
+
+    self.shoot = function(angle)
+    {
+        var b = Bullet(self.id, angle)
+        b.x = self.x
+        b.y = self.y
+
+    }
+
     self.updatePosition = function()
     {
-        if(self.right)
-        {
-            self.x += self.speed
-        }
-        if(self.left)
-        {
-            self.x -= self.speed
-        }
-        if(self.up)
-        {
-            self.y -= self.speed
-        }
-        if(self.down)
-        {
-            self.y += self.speed
-        }
+        self.x += self.spX
+        self.y += self.spY
+    }
+    self.getDist = function(point)
+    {
+        return Math.sqrt(Math.pow(self.x - point.x, 2) + Math.pow(self.y - point.y, 2))
     }
 
     return self
 }
 
+var Player = function(id)
+{
+    var self = GameObject()
+    self.id = id
+    self.number = Math.floor(Math.random() * 10)
+    self.right = false
+    self.left = false
+    self.up = false
+    self.down = false
+    self.attack = false
+    self.mouseAngle = 0
+    self.speed = 10
+
+    var playerUpdate = self.update
+
+    self.update = function()
+    {
+        self.updateSpeed()
+        playerUpdate()
+        if(self.attack)
+        {
+            self.shoot(self.mouseAngle)
+        }
+        // if(Math.random() < 0.1)
+        // {
+        //     self.shoot(Math.random() * 360)
+        // }
+    }
+
+    self.updateSpeed = function()
+    {
+        // left and right
+        if(self.right)
+        {
+            self.spX = self.speed
+        }
+        else if(self.left)
+        {
+            self.spX = -self.speed
+        }
+        else
+        {
+            self.spX = 0;
+        }
+        // up and down
+        if(self.up)
+        {
+            self.spY = -self.speed
+        }
+        else if(self.down)
+        {
+            self.spY = self.speed
+        }
+        else
+        {
+            self.spY = 0;
+        }
+    }
+
+    Player.list[id] = self
+
+    return self
+}
+
+Player.list = {}
+
+// list of functions for player connection and movement
+Player.onConnect = function(socket)
+{
+    var player = new Player(socket.id)
+        // receive the player inputs
+        socket.on('keypress', function(data)
+        {
+            if(data.inputId === 'up')
+            {
+                player.up = data.state
+            }
+            if(data.inputId === 'down')
+            {
+                player.down = data.state
+            }
+            if(data.inputId === 'left')
+            {
+                player.left = data.state
+            }
+            if(data.inputId === 'right')
+            {
+                player.right = data.state
+            }
+            if(data.inputId === 'attack')
+            {
+                player.attack = data.state
+            }
+            if(data.inputId === 'mouseAngle')
+            {
+                player.mouseAngle = data.state
+            }
+        })
+}
+
+Player.onDisconnect = function(socket)
+{
+    delete Player.list[socket.id]
+}
+
+Player.update = function()
+{
+    var pack = []
+    for(var i in Player.list)
+    {
+        var player = Player.list[i]
+        player.update()   
+        pack.push(
+            {
+                x:player.x,
+                y:player.y,
+                number:player.number
+            })  
+    }
+
+    return pack
+}
+
+var Bullet = function(parent, angle)
+{
+    var self = GameObject()
+    self.id = Math.random()
+    self.spX = Math.cos(angle/180 * Math.PI) * 10
+    self.spY = Math.sin(angle/180 * Math.PI) * 10
+    self.parent = parent
+
+    self.timer = 0;
+    self.toRemove = false
+
+    var bulletUpdate = self.update
+    self.update = function()
+    {
+        if(self.timer++ > 100)
+        {
+            self.toRemove = true
+        }
+        bulletUpdate()
+        for(var i in Player.list)
+        {
+            var p = Player.list[i]
+            if(self.getDist(p) < 25 && self.parent !== p.id)
+            {
+                self.toRemove = true
+                // damage player hp
+            } 
+        }
+    }
+    Bullet.list[self.id] = self
+    return self
+}
+
+Bullet.list = {}
+
+Bullet.update = function()
+{
+    //if(Math.random() < 0.1)
+    //{
+    //    Bullet(Math.random() * 360)
+    //}
+    var pack = []
+    for(var i in Bullet.list)
+    {
+        var bullet = Bullet.list[i]
+        bullet.update()   
+
+        if(bullet.toRemove)
+        {
+            delete Bullet.list[i]
+        }
+        else
+        {
+            pack.push(
+                {
+                    x:bullet.x,
+                    y:bullet.y,
+                })  
+        }
+ 
+    }
+
+    return pack
+}
+// connection to game
 io.sockets.on('connection', function(socket)
 {
     console.log("Socket Connected")
@@ -67,37 +257,35 @@ io.sockets.on('connection', function(socket)
 
     //add something to SocketList
     SocketList[socket.id] = socket
+    Player.onConnect(socket)
 
 
-    var player = new Player(socket.id)
-    PlayerList[socket.id] = player
+
 
     // disconnection event
     socket.on("disconnect", function()
     {
         delete SocketList[socket.id]
-        delete PlayerList[socket.id]
+        Player.onDisconnect(socket)
     })
 
-    // receive the player inputs
-    socket.on('keypress', function(data)
+    // handling chat event
+    socket.on("sendMessageToServer", function(data)
     {
-        if(data.inputId === 'up')
+        var playerName = (" " + socket.id).slice(2,7)
+        for(var i in SocketList)
         {
-            player.up = data.state
+            SocketList[i].emit('addToChat', playerName + ": " + data)
         }
-        if(data.inputId === 'down')
+    })
+    socket.on("evalServer", function(data)
+    {
+        if(!debug)
         {
-            player.down = data.state
+            return
         }
-        if(data.inputId === 'left')
-        {
-            player.left = data.state
-        }
-        if(data.inputId === 'right')
-        {
-            player.right = data.state
-        }
+        var res = eval(data)
+        socket.emit('evalResponse', res)
     })
 
     /// old examples from wednesday 1/27
@@ -119,18 +307,14 @@ io.sockets.on('connection', function(socket)
 // setup update loop
 setInterval(function()
 {
-    var pack = []
-    for(var i in PlayerList)
+    var pack = 
     {
-        var player = PlayerList[i]
-        player.updatePosition()   
-        pack.push(
-            {
-                x:player.x,
-                y:player.y,
-                number:player.number
-            })  
+        player: Player.update(),
+        bullet: Bullet.update()
+
     }
+    //var pack = Player.update()
+    //Player.update()
 
     for(var i in SocketList)
     {
--- week3/client/js/game.js
+++ week3/client/js/game.js
@@ -1,27 +1,35 @@
 var socket = io()
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
+var chatText = document.getElementById('chat-text')
+var chatInput = document.getElementById('chat-input')
+var chatForm = document.getElementById('chat-form')
 ctx.font = "30px Arial"
+var px
+var py
 
-//event listeners for keypresses
+//event listeners for keypresses and mouse clicks / mouse position
 document.addEventListener('keydown', keyPressDown)
 document.addEventListener('keyup', keyPressUp)
+document.addEventListener('mousedown',mouseDown)
+document.addEventListener('mouseup',mouseUp)
+document.addEventListener('mousemove',mouseMove)
 
 function keyPressDown(e)
 {
-    if(e.keyCode === 38) // up
+    if(e.keyCode === 87) // up
     {
         socket.emit("keypress", {inputId:'up', state:true})
     }
-    else if(e.keyCode === 40) // down
+    else if(e.keyCode === 83) // down
     {
         socket.emit("keypress", {inputId:'down', state:true})
     }
-    else if(e.keyCode === 37) // left
+    else if(e.keyCode === 65) // left
     {
         socket.emit("keypress", {inputId:'left', state:true})
     }
-    else if(e.keyCode === 39) // right
+    else if(e.keyCode === 68) // right
     {
         socket.emit("keypress", {inputId:'right', state:true})
     }
@@ -29,32 +37,84 @@ function keyPressDown(e)
 
 function keyPressUp(e)
 {
-    if(e.keyCode === 38) // up
+    if(e.keyCode === 87) // up
     {
         socket.emit("keypress", {inputId:'up', state:false})
     }
-    else if(e.keyCode === 40) // down
+    else if(e.keyCode === 83) // down
     {
         socket.emit("keypress", {inputId:'down', state:false})
     }
-    else if(e.keyCode === 37) // left
+    else if(e.keyCode === 65) // left
     {
         socket.emit("keypress", {inputId:'left', state:false})
     }
-    else if(e.keyCode === 39) // right
+    else if(e.keyCode === 68) // right
     {
         socket.emit("keypress", {inputId:'right', state:false})
     }
 }
 
+function mouseDown(e)
+{
+    socket.emit('keypress',{inputId: 'attack', state: true})
+}
+
+function mouseUp(e)
+{
+    socket.emit('keypress', {inputId: 'attack', state: false})
+}
+
+function mouseMove(e)
+{
+    var x = -canvas.width/2 + e.clientX
+    //var x = -px[i] + e.clientX
+    var y = -canvas.height/2 + e.clientY
+    //var y = player.y + e.clientY
+    var angle = Math.atan2(y,x) / Math.PI * 180
+    socket.emit('keypress', {inputId: 'mouseAngle', state: angle})
+}
+
 socket.on('newPosition', function(data)
 {
     ctx.clearRect(0,0, canvas.width, canvas.height)
-    for(var i = 0; i < data.length; i++)
+    for(var i = 0; i < data.player.length; i++)
     {
-        ctx.fillText(data[i].number, data[i].x, data[i].y)
-    }           
+        
+        ctx.fillText(data.player[i].number, data.player[i].x, data.player[i].y)
+    }   
+    for(var i = 0; i < data.bullet.length; i++)
+    {
+        ctx.fillRect(data.bullet[i].x, data.bullet[i].y, 10, 10)
+    }        
 })
+
+socket.on('addToChat',function(data)
+{
+    chatText.innerHTML += `<div>${data}</div>`
+})
+
+socket.on('evalResponse',function(data)
+{
+    chatText.innerHTML += `<div>${data}</div>`
+    console.log(data)
+})
+
+chatForm.onsubmit = function(e)
+{
+    e.preventDefault()
+
+    if(chatInput.value[0]==='/')
+    {
+        socket.emit('evalServer',chatInput.value.slice(1))
+    }
+    else
+    {
+        socket.emit('sendMessageToServer', chatInput.value)
+    }
+    // clear out the input field
+    chatInput.value = ""
+}
 
 /// example code from wednesday 1/27
 // var msg = function()
